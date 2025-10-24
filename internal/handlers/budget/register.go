f6c2d803eb42ab6fbf6ec24a0c08c0a23d4621d4
Nikita Rasskazov <frosnikny@yandex.ru>
2025-10-24 03:17:41 +0300

    Remove unused endpoints and clean up API

--- internal/handlers/budget/register.go
+++ internal/handlers/budget/register.go
@@ -2,13 +2,8 @@ package budget
 
 import (
 	"github.com/gorilla/mux"
-
-	"net/http"
 )
 
 func Register(r *mux.Router, uc BudgetUseCase) {
-	handler := NewHandler(uc)
-
-	r.HandleFunc("/budgets", handler.GetListBudgets).Methods(http.MethodGet)
-	r.HandleFunc("/budget/{id}", handler.GetBudgetByID).Methods(http.MethodGet)
+	// Не используется фронтом
 }
